3903c207b1c6dacd37a42b28db001b1e3d916b95
netwalkersng <devs.netwalkersng@gmail.com>
2019-07-29 03:42:56 -0700

    completed desktop view

--- application/views/Home.php
+++ application/views/Home.php
@@ -313,9 +313,47 @@
       </div>  
     </main>
 
-    <footer>
-      <div data-footer>
-        footer
+    <footer data-footer>
+      <div data-footer-row1>
+        <div data-col1>
+          <h4><span>About</span> Us</h4>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor.
+
+          <p class="subheading">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+        </p>
+        </div>
+
+        <div data-col2>
+          <h4> <span>Our</span> Hours</h4>
+            <ul>
+              <li>Monday <span>08:00 - 19:00</span></li>
+              <li>Tuesday <span>08:00 - 19:00</span></li>
+              <li>Wednessday <span>08:00 - 19:00</span></li>
+              <li>Thursday<span>08:00 - 19:00</span></li>
+              <li>Friday <span>08:00 - 19:00</span></li>
+              <li>Saturday<span>08:00 - 19:00</span></li>
+              <li>Sunday <span>Closed</span></li>
+            </ul>
+        </div>
+        
+ 
+ 
+
+        <div data-col3>
+          <h4><span>Our</span> Contacts</h4>
+           
+              <p><i class="fa fa-map" aria-hidden="true"></i>Lorem ipsum Dolor sit amet, 658 Consectetur, Adipisicing 56 D </p>
+              <p><i class="fa fa-phone" aria-hidden="true"></i> +234 7087429861   </p>
+              <p> <i class="fa fa-envelope" aria-hidden="true"></i>support@motosellers.com</p>
+            
+        </div>
+
+        <div data-col4><h4>Our Location</h4></div>
+      </div>
+      <div data-col5>
+          Copyright © 2015 - 2019. Motoseller.com. All rights reserved <span>Developed by	Netwalkers NG</span>
       </div>
     </footer>
 
